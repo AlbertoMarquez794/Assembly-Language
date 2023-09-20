a599351952b2e402b74b1146fb34555f716f2599
BRAYAN ALBERTO CHAVEZ MARQUEZ <bchavezm@itam.mx>
2023-09-19 23:02:48 -0600

    Updating files

--- Led_interrupt/Led_interrupt/main.asm
+++ Led_interrupt/Led_interrupt/main.asm
@@ -1,75 +1,50 @@
-;
-; Led_interrupt.asm
-;
-; Created: 9/13/2023 10:55:29 AM
-; Author : hugor
-;
+.include "m328pdef.inc" ; Incluye el archivo de definición del microcontrolador ATmega328P.
 
+.org 0x00 ; Establece la dirección de inicio del programa en 0x00.
+     jmp main ; Salta a la etiqueta "main".
 
-; Replace with your application code
-.include "m328pdef.inc"
-.org 0x00
-     jmp main
-.org OVF1addr
-     jmp TIM1_OVF
+.org OVF1addr ; Establece una dirección específica para la rutina de interrupción de desbordamiento del Timer1, llamada "TIM1_OVF".
+     jmp TIM1_OVF ; Salta a la rutina de interrupción cuando ocurre un desbordamiento del Timer1.
 
-.org 0x40
-main: call serial_init
-      ldi r16, high(ramend)
-      out sph, r16
-      ldi r16, low(ramend)
-      out spl, r16
+.org 0x40 ; Inicia el programa principal a partir de la dirección 0x40.
 
-      sbi ddrb, 5
-      sbi portb, 5
+main: ; Etiqueta "main" donde comienza el programa principal.
+      call serial_init ; Llama a la función "serial_init", que probablemente configure la comunicación serial.
+      ldi r16, high(ramend) ; Carga el byte alto de la dirección final de la memoria RAM en r16.
+      out sph, r16 ; Escribe el valor de r16 en el registro sph, configurando el límite superior de la memoria RAM.
+      ldi r16, low(ramend) ; Carga el byte bajo de la dirección final de la memoria RAM en r16.
+      out spl, r16 ; Escribe el valor de r16 en el registro spl, configurando el límite inferior de la memoria RAM.
 
-	  ldi r20, 0x00
-	  ldi r21, 0x05
-	  sts TCCR1A, r20
-	  sts TCCR1B, r21
+      sbi ddrb, 5 ; Configura el bit 5 del registro ddrb como salida.
+      sbi portb, 5 ; Establece en alto (enciende) el bit 5 del puerto B.
 
-	  ldi r20, 0x00
-	  ldi r20, (1<<TOIE1)
-	  sts TIMSK1, r20
+	  ldi r20, 0x00 ; Carga el valor 0x00 en r20.
+	  ldi r21, 0x05 ; Carga el valor 0x05 en r21.
+	  sts TCCR1A, r20 ; Escribe el valor de r20 (00000000) en el registro TCCR1A.
+	  sts TCCR1B, r21 ; Escribe el valor de r21 (00000101) en el registro TCCR1B.
 
-	  sei 
+	  ldi r20, 0x00 ; Carga el valor 0x00 en r20 (esto parece ser un error, debería ser r21 en lugar de r20).
+	  ldi r20, (1<<TOIE1) ; Carga en r20 el resultado de la operación (1<<TOIE1), que configura la interrupción de desbordamiento del Timer1.
+	  sts TIMSK1, r20 ; Escribe el valor de r20 en el registro TIMSK1 para habilitar la interrupción de desbordamiento del Timer1.
 
-	  ldi r20, 0xFF
-      ldi r21, 0x3C
-      sts TCNT1H,r20
-      sts TCNT1L,r21
+	  sei ; Habilita las interrupciones globales.
 
-      ldi r16, 32
-      out portb, r16
+	  ldi r20, 0xFF ; Carga el valor 0xFF en r20.
+      ldi r21, 0x3C ; Carga el valor 0x3C en r21.
+      sts TCNT1H, r20 ; Escribe el valor de r20 en el registro TCNT1H del Timer1.
+      sts TCNT1L, r21 ; Escribe el valor de r21 en el registro TCNT1L del Timer1.
+	  ;¿Preguntar?
+      ldi r16, 32 ; Carga el valor 32 en r16.
+      out portb, r16 ; Escribe el valor de r16 en el puerto B, encendiendo el LED conectado al pin 5.
 
-here: 
-      jmp here
+here: ; Etiqueta "here" que marca el inicio de un bucle infinito.
+      jmp here ; Salta de nuevo a la etiqueta "here", creando un bucle infinito.
 
+.org 0x0200 ; Establece la dirección para la rutina de interrupción "TIM1_OVF".
 
-.org 0x0200
-TIM1_OVF :  in r16, portb
-            ldi r17, 32
-			eor r16, r17
-	        out portb, r16
-			reti
-
-
-			;initialise serial connection
-serial_init:
-         ldi r16, 103
-         clr r17
-         sts 0xc5, r17
-         sts 0xc4, r16
-         ldi r16, (1<<4)|(1<<3)
-         sts 0xc1, r16
-         ldi r16, 0b00001110
-         sts 0xc2, r16
-         ret
-;transmit byte stored in r19 over serial
-serial_transmit:
-         lds r16, 0xc0
-         sbrs r16, 5
-         rjmp serial_transmit
-         sts 0xc6, r19
-         ret
-;wait for byte to be sent, store it in r19 and return
+TIM1_OVF : ; Etiqueta para la rutina de interrupción "TIM1_OVF".
+      in r16, portb ; Lee el estado del puerto B y lo coloca en r16.
+      ldi r17, 32 ; Carga el valor 32 en r17.
+      eor r16, r17 ; Realiza una operación XOR entre r16 y r17.
+      out portb, r16 ; Escribe el resultado de la operación en el puerto B.
+      reti ; Retorna de la rutina de interrupción.
--- Led_print_interrupt/Led_print_interrupt/main.asm
+++ Led_print_interrupt/Led_print_interrupt/main.asm
@@ -10,14 +10,16 @@
 .include "m328pdef.inc"
 .org 0x00
      jmp main
+
+;Configuracion de las interrepciones de overflow de temporizadores 0 y 1
 .org OVF0addr
      jmp TIM0_OVF
 .org OVF1addr
      jmp TIM1_OVF
 
 .org 0x40
-main: call serial_init
-
+main: 
+	call serial_init
 	ldi r16, high(ramend) ;Carga el valor alto de la direccion de memoria RAM
 	out sph, r16 ;Almacena el contenido de r16 en el registro SPH
 	ldi r16, low(ramend) ;Carga el valor bajon de la direccion de memoria RAM en r16
@@ -38,37 +40,60 @@ main: call serial_init
 	; Almacenar los valores en los registros TCNT1H y TCNT1L del temporizador 1
 	sts TCNT1H, r20 ;TCNT1H = 255
 	sts TCNT1L, r21 ;TCNT1L = 60
-
+	;TCNT1H 1 1 1 1 1 1 1 1
+	;TCNT1L 1 1 1 1 0 0 0 0
+	;En este caso, se carga 255 (0xFF) en TCNT1H y 60 (0x3C) en TCNT1L. 
+	;Esto significa que el temporizador 1 comenzará a contar desde 0xFF3C 
+	;(255 en TCNT1H y 60 en TCNT1L) y contará hacia arriba hasta 0xFFFF
+	;antes de desbordarse.
 	; Cargar el valor 0xFD en el registro r23
 	ldi r23, 0xFD ;r23 = 253
 
 	; Escribir el valor en el registro TCNT0 del temporizador 0
-	out TCNT0, r23
-
+	out TCNT0, r23 ;TCNT0 = 0000 0000l 
+	;++++++++++++++++++++++++++++++++++++++++++
+	;Todo esto es del contador 1
+	;Este parte es la que enciende el led
 	ldi r16, 32          ; Cargar el valor 32 en el registro r16
 	out portb, r16       ; Escribir el valor de r16 en el registro PORTB
+	;Escribir el valor 32 en el registro 16. El bit 5 de PORTB se establece en alto, lo que significa que el LED
+	;conectado al 
     ldi r16, 16          ; Cargar el valor 16 en el registro r16
     out portd, r16       ; Escribir el valor de r16 en el registro PORTD
 	;TCCR1A-TCCR1B, TCCR1C son registros de control del Timer1/Timer2
 	ldi r20, 0x00        ; Cargar el valor 0 en el registro r20
-	ldi r21, (1<<CS12)|(1<<CS10) ; Cargar el valor binario 00001001 (9) en r21
+	ldi r21, (1<<CS12)|(1<<CS10) ; Cargar el valor binario 00000101 (9) en r21
 	sts TCCR1A, r20      ; Almacenar el valor de r20 en el registro TCCR1A 0
 	sts TCCR1B, r21      ; Almacenar el valor de r21 en el registro TCCR1B 9
-
+	; 0 0 0 0 0 1 0 1
+	; CS12 = 1
+	; CS10 = 1
+	; Caso = 101
+	;clk/1024 (from prescaler)7
+	; El TCCR1B se configura como un prescaler en 1024. El timer 1 contará pulsos de reloj del sistema divididos por un factor de 1024
+	;++++++++++++++++++++++++++++++++++++++++++++
 	ldi r20, 0x00        ; Cargar el valor 0 en el registro r20
 	out TCCR0A, r20      ; Escribir el valor de r20 en el registro TCCR0A (Timer/Counter 1 Control Register A) registro para controlar el Timer/Counter1
 	ldi r21, (1<<CS02)|(1<<CS01) ; Cargar el valor binario 00000110 en r21
+	;	CS02 = 1
+	; CS01 = 1
+	; 0 0 0 0 0 1 1 0
+	; El timer 0 se esta usando como contador y obtiene pulsos de una fuente externa
 	out TCCR0B, r21      ; Escribir el valor de r21 en el registro TCCR0B, 6
-
+	;++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	;Esta parte es la interrùpción
 	ldi r20, 0x00        ; Cargar el valor 0 en el registro r20
+	;Normal port operation, OC0A disconnected
+	;Estás deshabilitando todas las funciones especiales
 	ldi r20, (1<<TOIE0)  ; Cargar el valor binario 00000001 en r20
+	; 0 0 0 0 0 0 0 1
 	sts TIMSK0, r20      ; Almacenar el valor de r20 en el registro TIMSK0
+	;activa la interrupción
 	;TIMSK0: este registro es para activar las interrupciones. 
 	;Si el bit 0 está activo signifca que está permitido las interrupciones de overflow. 
 	;Si el bit 1 está activo, significa que cuando se compare el valor del OCR0A y TCNT0, y sean iguales, 
 	;entonces se disparará una interrupción. Igualmente si está en 1 el bit 3, ocurre lo mismo pero con el OCR0B.
 	;relacionadas con el Timer/Counter0 (Temporizador/Contador 0).
-
 	;+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	ldi r20, 0x00        ; Cargar el valor 0 en el registro r20
 	ldi r20, (1<<TOIE1)  ; Cargar el valor binario 00000001 en r20
@@ -82,14 +107,17 @@ main: call serial_init
 	here:
 		jmp here             ; Bucle infinito, salta a sí mismo repetidamente
 
+	;Ocurre cuando hay oveflow del temporizador 1
 	.org 0x0100
 	TIM0_OVF:
 		in r16, portb        ; Leer el valor del registro PORTB en r16
 		ldi r17, 32          ; Cargar el valor 32 en el registro r17
 		eor r16, r17         ; Realizar una operación XOR entre r16 y r17
+		;Cambias el quinto bit de estado alto a bajo y viceversa
 		out portb, r16       ; Escribir el resultado de vuelta en el registro PORTB
-		ldi r23, 0xFD        ; Cargar el valor 0xFD en r23
-		out TCNT0, r23       ; Escribir el valor de r23 en el registro TCNT0
+		ldi r23, 0xFD        ; Cargar el valor 0xFD (253, 1111 1101 ) en r23
+		out TCNT0, r23       ; Escribir el valor de r23 en el registro TCNT0 que es 254 y aumentará con cada puso de roloj hasta que alcance su valor maximo
+		;255 y luego se desbordará
 		ldi r18, 0           ; Cargar 0 en r18 (posiblemente para algún propósito futuro)
 		reti                 ; Retornar de la interrupción
 
